a0bfb435304760827e10395fb3fa41e690b71c31
Niki Hite <nikihite@comcast.net>
2022-08-23 12:52:58 -0700

    sign up form, event listener, fetch-utils

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 // import functions and grab DOM elements
-
+import { signUpUser } from './fetch-utils.js';
 // let state
-
+const signUpForm = document.getElementById('sign-up-form');
 // set event listeners 
+signUpForm.addEventListener('submit', async (e) => {
+    e.preventDefault();
+    const formData = new FormData(signUpForm);
+    await signUpUser({
+        email: formData.get('email'),
+        password: formData.get('password'),
+        firstName: formData.get('first-name'),
+        lastName: formData.get('last-name'),
+    });
+});
   // get user input
   // use user input to update state 
   // update DOM to reflect the new state
