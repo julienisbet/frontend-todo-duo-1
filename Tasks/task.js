174e4ee2271dfebe46a8bc324df572d431eacf96
Amanda Michelle Hecht <amandahecht89@gmail.com>
2022-08-24 18:50:32 -0600

    button but it doesnt work

--- Tasks/task.js
+++ Tasks/task.js
@@ -1,9 +1,15 @@
 import { checkUser, logoutUser, getTodo, toggleTodo } from '../fetch-utils.js';
 
 checkUser();
+
 const logout = document.getElementById('logout');
 logout.addEventListener('click', async () => {
     await logoutUser();
+});
+
+const addChore = document.getElementById('addChore');
+logout.addEventListener('click', async () => {
+    await addChore();
 });
 
 let toDo = [];
